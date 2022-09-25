2773ea8c27a4cb44c69b5b354e93a742356b815b
naderghazi <nader@defraged.com>
2022-09-25 22:47:20 +0330

    feat: api specification page

---
+++ resources/views/specification.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <script src="https://unpkg.com/@stoplight/elements/web-components.min.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/@stoplight/elements/styles.min.css">
+    <title>API specification</title>
+</head>
+<body class="antialiased">
+<elements-api
+    apiDescriptionUrl="{{asset('specification.yaml')}}"
+    router="hash"
+></elements-api>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/specification', function () {
+    return view('specification');
 });
