cb099e923c3d43af2da210a41fd08f7a0528848d
naderghazi <nader@defraged.com>
2022-09-25 18:47:06 +0330

    refactor: TDEE calculator

--- app/Enums/ActivityLevel.php
+++ app/Enums/ActivityLevel.php
@@ -4,9 +4,9 @@ namespace App\Enums;
 
 enum ActivityLevel: string
 {
-    case SEDENTARY = '1.2';
-    case LIGHTLY_ACTIVE = '1.375';
-    case MODERATELY_ACTIVE = '1.55';
-    case VERY_ACTIVE = '1.725';
-    case EXTREMELY_ACTIVE = '1.9';
+    case SEDENTARY = 'sedentary';
+    case LIGHTLY_ACTIVE = 'lightly_active';
+    case MODERATELY_ACTIVE = 'moderately_active';
+    case VERY_ACTIVE = 'very_active';
+    case EXTREMELY_ACTIVE = 'extremely_active';
 }
--- app/Services/TDEECalculator.php
+++ app/Services/TDEECalculator.php
@@ -9,6 +9,14 @@ class TDEECalculator
 
     public function calculate(int $bmr, ActivityLevel $activityLevel): int
     {
-        return ceil($bmr * $activityLevel->value);
+        $activityLevelValue = match ($activityLevel) {
+            ActivityLevel::SEDENTARY => 1.2,
+            ActivityLevel::LIGHTLY_ACTIVE => 1.375,
+            ActivityLevel::MODERATELY_ACTIVE => 1.55,
+            ActivityLevel::VERY_ACTIVE => 1.725,
+            ActivityLevel::EXTREMELY_ACTIVE => 1.9,
+        };
+
+        return ceil($bmr * $activityLevelValue);
     }
 }
